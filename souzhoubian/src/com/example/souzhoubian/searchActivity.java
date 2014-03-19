002874234470f0a2bb229a9548f9831cba4a756f
胥青远 <xqy91719@gmail.com>
2014-03-19 10:42:10 +0800

    增加searchActivity类

---
+++ souzhoubian/src/com/example/souzhoubian/searchActivity.java
@@ -0,0 +1,34 @@
+package com.example.souzhoubian;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: x
+ * Date: 14-3-19
+ * Time: 上午9:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class searchActivity extends Activity {
+    private ImageButton goBack,search;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        goBack = (ImageButton) findViewById(R.id.search_go_back);
+        search = (ImageButton) findViewById(R.id.getsearch);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(searchActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
